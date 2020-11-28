2e645cb424dcca99a44a0fb309d571cbb2b30f05
Steven Cook <steve.j.cook@gmail.com>
2020-11-28 19:03:01 +1000

    Added Python based Lambda

--- bin/cdk-lambda-deploy.ts
+++ bin/cdk-lambda-deploy.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 import * as cdk from '@aws-cdk/core';
 import { CdkLambdaDeployStack } from '../lib/cdk-lambda-deploy-stack';
+import { CdkPythonLambdaDeployStack } from '../lib/cdk-py-lambda-deploy';
 
 const app = new cdk.App();
 new CdkLambdaDeployStack(app, 'CdkLambdaDeployStack');
+new CdkPythonLambdaDeployStack(app, 'CdkPythonLambdaDeployStack');
--- lib/cdk-py-lambda-deploy.ts
+++ lib/cdk-py-lambda-deploy.ts
@@ -5,38 +5,38 @@ import * as codeDeploy from '@aws-cdk/aws-codedeploy';
 import { PythonFunction } from '@aws-cdk/aws-lambda-python';
 
 
-export class CdkLambdaDeployStack extends cdk.Stack {
+export class CdkPythonLambdaDeployStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // Simple Lambda function that we will update and redeploy to test 
-    // the ability to use Code Deploy managed canary deployments. 
-    const hello = new lambda.Function(this, 'HelloHandler', {
-      runtime: lambda.Runtime.NODEJS_12_X,
-      code: lambda.Code.fromAsset('lambda/js/'),
-      handler: 'hello.handler',
+    // Simple Python based Lambda function to test packaging with pip dependencies. 
+    const ipLambda = new PythonFunction(this, 'IPLambda', {
+        entry: 'lambda/py/',
+        index: 'app.py',
+        handler: 'lambda_handler',
+        runtime: lambda.Runtime.PYTHON_3_8
     });
 
     // Define an Alias for the Lambda. 
     // This allows traffic to be shifted between Lambda versions
     const alias = new lambda.Alias(this, 'live', {
       aliasName: 'live',
-      version: hello.currentVersion,
+      version: ipLambda.currentVersion,
     });
 
     // API Gateway points to the Lambda Alias, not directly the to Function. 
     // Use default settings to proxy everthing to the lambda. 
-    new apigw.LambdaRestApi(this, 'Endpoint', {
+    new apigw.LambdaRestApi(this, 'IPLambdaEndpoint', {
       handler: alias
     });
 
     // Create the Code Deploy setup to manage the Canary deployment. 
-    const lambdaApp = new codeDeploy.LambdaApplication(this, 'HelloApplicaiton');
-    new codeDeploy.LambdaDeploymentGroup(this, 'HelloDeploymentGroup', {
+    const lambdaApp = new codeDeploy.LambdaApplication(this, 'IPLambdaApplicaiton');
+    new codeDeploy.LambdaDeploymentGroup(this, 'IPLambdaDeploymentGroup', {
       alias: alias,
       application: lambdaApp,
       deploymentConfig: codeDeploy.LambdaDeploymentConfig.CANARY_10PERCENT_5MINUTES,
-      deploymentGroupName: 'HelloLambdaDeploy'
+      deploymentGroupName: 'IPLambdaLambdaDeploy'
     });
 
   }
